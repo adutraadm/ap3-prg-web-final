1c5455046b9f047eada2c15a893eb5135c790c87
Anderson Dos Santos Dutra <adutra@br.ibm.com>
2020-12-03 23:57:23 -0300

    first commit

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "O que é trader ?",
+	"option1": "um esporte",
+	"option2": "Modalidade de investimento em ações",
+	"option3": "Doença",
+	"option4": "Tipo de Veiculos",
+	"answer": "2"
+}, {
+	"question": "O que é Quiropraxia ?",
+	"option1": "Um tipo de doença",
+	"option2": "Transtorno mental",
+	"option3": "forma de medicina alternativa",
+	"option4": "Droga Sintetica",
+	"answer": "3"
+}, {
+	"question": "O que é virabrequim ?",
+	"option1": "Peça de um motor",
+	"option2": "Estrumento musical",
+	"option3": "Tipo de terremoto",
+	"option4": "Movimento de luta",
+	"answer": "1"
+}, {
+	"question": "C# é ?",
+	"option1": "Uma linguagem de programação",
+	"option2": "Uma forma de chingamento",
+	"option3": "Elemento da tabela periodica",
+	"option4": "Nenhuma das opções",
+	"answer": "1"
+}, {
+	"question": "O que é via lactea ?",
+	"option1": "Marca de leite",
+	"option2": "Carro",
+	"option3": "Galaxia",
+	"option4": "Civilização antiga",
+	"answer": "3"
+}, {
+	"question": "Hipertropia",
+	"option1": "Envolve o aumento do músculo através do crescimento",
+	"option2": "Contusão",
+	"option3": "Tipo de exame",
+	"option4": "Figura de linguage",
+	"answer": "1"
+}, {
+	"question": "Assintomático",
+	"option1": "Pessoa de bom humor",
+	"option2": "Guia espiritual",
+	"option3": "Pessoas com o vírus, mas sem sintomas",
+	"option4": "Que não tem fé na religião",
+	"answer": "3"
+}, {
+	"question": "ácido acetilsalicílico é a formula do ?",
+	"option1": "AAS",
+	"option2": "Dipirona",
+	"option3": "Dorflex",
+	"option4": "Soda caustica",
+	"answer": "1"
+}, {
+	"question": "Piseiro",
+	"option1": "Doença dos pés ",
+	"option2": "Descontrole de multidão",
+	"option3": "Estilo musical",
+	"option4": "Tipo de planta",
+	"answer": "3"
+}, {
+	"question": "O que é cigarro eletronico?",
+	"option1": "Cigarro que solta fumaça de curto circuito",
+	"option2": "Cigarro digital sé existe no computador",
+	"option3": "Uma opção de cigarro que é similar ao cigarro tradicional mais não faz mau",
+	"option4": "Cigarro que dá choque",
+	"answer": "3"
+}];
